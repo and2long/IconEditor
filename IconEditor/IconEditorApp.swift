4573328d6d9e66ec989129e52e8df0d97e906920
Lilong Zhang <and2long@gmail.com>
2024-12-29 10:54:19 +0800

    Initial Commit

---
+++ IconEditor/IconEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconEditorApp.swift
+//  IconEditor
+//
+//  Created by Lilong Zhang on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct IconEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
